072c7bd2c7eed94ab3dcfe43dc2a2ab9d85d7ac7
kchatdd <62733349+kchatdd@users.noreply.github.com>
2024-01-24 20:33:53 -0600

    added menu items to app

--- onlineMenu/js/app.js
+++ onlineMenu/js/app.js
@@ -19,7 +19,7 @@ let menuItems = [
         id: 1,
         type: "appetizers",
         item: "salt fish fritters",
-        descripton: "fried salt fish fritters with chili dipping sauce", 
+        desc: "fried salt fish fritters with chili dipping sauce", 
         imgUrl: "saltfishfritters.jpg", 
         price: 8.99,
         qty: 0,
@@ -28,8 +28,8 @@ let menuItems = [
         id: 2,
         type: "appetizers",
         item: "cream of callaloo soup",
-        descripton: "vegetable-based soup", 
-        imgUrl: "images/callaloosoup.jpg", 
+        desc: "vegetable-based soup", 
+        imgUrl: "callaloosoup.jpg", 
         price: 6.99,
         qty: 0,
     },
@@ -37,8 +37,8 @@ let menuItems = [
         id: 3,
         type: "appetizers",
         item: "ackee and saltfish plantain poppers",
-        descripton: "ackee and saltfish served in fried plantain cups", 
-        imgUrl: "images/acksaltpop.jpg", 
+        desc: "ackee and saltfish served in fried plantain cups", 
+        imgUrl: "acksaltpop.jpg", 
         price: 8.99,
         qty: 0,
     },
@@ -46,8 +46,8 @@ let menuItems = [
         id: 4,
         type: "appeitzers",
         item: "mini beef patties",
-        descripton: "6 mini Jamacian beef patties", 
-        imgUrl: "images/minibeefpatties.jpg", 
+        desc: "6 mini Jamacian beef patties", 
+        imgUrl: "minibeefpatties.jpg", 
         price: 9.99,
         qty: 0,
     },
@@ -55,8 +55,8 @@ let menuItems = [
         id: 5,
         type: "entrees",
         item: "Oxtails",
-        descripton: "Oxtails with rice adn plantains", 
-        imgUrl: "images/oxtails.jpg", 
+        desc: "Oxtails with rice adn plantains", 
+        imgUrl: "oxtails.jpg", 
         price: 22.99,
         qty: 0,
     },
@@ -64,8 +64,8 @@ let menuItems = [
         id: 6,
         type: "entrees",
         item: "Curry Goat",
-        descripton: "Curry Goat with rice and peas, cabbage, and plantains", 
-        imgUrl: "images/currygoat.jpg", 
+        desc: "Curry Goat with rice and peas, cabbage, and plantains", 
+        imgUrl: "currygoat.jpg", 
         price: 20.99,
         qty: 0,
     },
@@ -73,8 +73,8 @@ let menuItems = [
         id: 7,
         type: "entrees",
         item: "Fried Cabbage",
-        descripton: "Sauteed cabbage with rice and peas and plantains", 
-        imgUrl: "images/cabbage.jpg", 
+        desc: "Sauteed cabbage with rice and peas and plantains", 
+        imgUrl: "cabbage.jpg", 
         price: 16.99,
         qty: 0,
     },
@@ -82,8 +82,8 @@ let menuItems = [
         id: 8,
         type: "entrees",
         item: "Jerk Shrimp",
-        descripton: "Sauteed jerk shrimp with rice and peas and plantains", 
-        imgUrl: "images/jerkshrimp.jpg", 
+        desc: "Sauteed jerk shrimp with rice and peas and plantains", 
+        imgUrl: "jerkshrimp.jpg", 
         price: 22.99,
         qty: 0,
     },
@@ -91,8 +91,8 @@ let menuItems = [
         id: 9,
         type: "drinks",
         item: "Ginger Beer",
-        descripton: "traditional ginger beer made fresh daily", 
-        imgUrl: "images/gingerbeer.jpg", 
+        desc: "traditional ginger beer made fresh daily", 
+        imgUrl: "gingerbeer.jpg", 
         price: 3.99,
         qty: 0,
     },
@@ -100,8 +100,8 @@ let menuItems = [
         id: 10,
         type: "drinks",
         item: "Sorrel (Hibiscus) Juice",
-        descripton: "hibiscus juice made fresh daily", 
-        imgUrl: "images/sorreljuice.jpg", 
+        desc: "hibiscus juice made fresh daily", 
+        imgUrl: "sorreljuice.jpg", 
         price: 3.99,
         qty: 0,
     },
@@ -109,8 +109,8 @@ let menuItems = [
         id: 11,
         type: "drinks",
         item: "Pineapple Ginger Juice",
-        descripton: "pineapple and ginger juice made fresh daily", 
-        imgUrl: "images/pineappleginger.jpg", 
+        desc: "pineapple and ginger juice made fresh daily", 
+        imgUrl: "pineappleginger.jpg", 
         price: 3.99,
         qty: 0,
     },
@@ -118,8 +118,8 @@ let menuItems = [
         id: 12,
         type: "drinks",
         item: "Can Soda",
-        descripton: "canned coca-cola soda", 
-        imgUrl: "images/cansoda.jpg", 
+        desc: "canned coca-cola soda", 
+        imgUrl: "cansoda.jpg", 
         price: 1.99,
         qty: 0,
     },
@@ -127,8 +127,8 @@ let menuItems = [
         id: 13,
         type: "desserts",
         item: "Sweet Potato Pudding",
-        descripton: "sweet potato pudding", 
-        imgUrl: "images/pudding.jpeg", 
+        desc: "sweet potato pudding", 
+        imgUrl: "pudding.jpeg", 
         price: 4.99,
         qty: 0,
     },
@@ -136,8 +136,8 @@ let menuItems = [
         id: 14,
         type: "desserts",
         item: "Gizzada",
-        descripton: "spiced coconut tart", 
-        imgUrl: "images/gizzada.jpg", 
+        desc: "spiced coconut tart", 
+        imgUrl: "gizzada.jpg", 
         price: 3.99,
         qty: 0,
     },
@@ -145,8 +145,8 @@ let menuItems = [
         id: 15,
         type: "desserts",
         item: "Rum Cake",
-        descripton: "rum cake", 
-        imgUrl: "images/rumcake.jpg", 
+        desc: "rum cake", 
+        imgUrl: "rumcake.jpg", 
         price: 4.99,
         qty: 0,
     },
@@ -154,14 +154,15 @@ let menuItems = [
         id: 16,
         type: "desserts",
         item: "Mango Cheesecake",
-        descripton: "spiced mango cheesecake", 
-        imgUrl: "images/mangocheesecake.jpg", 
+        desc: "spiced mango cheesecake", 
+        imgUrl: "mangocheesecake.jpg", 
         price: 4.99,
         qty: 0,
     }
 ]
 
 confirmBtn.addEventListener("click", (e)=> {
+    //preventDefault cancels any predetermeined action that would occur when the input/action is received
     e.preventDefault()
     //function that will calculate the total amt the customer pays
     getTotal();
@@ -198,6 +199,7 @@ menuDivs.forEach(div =>{
     const row = document.createElement("div");
     row.classList.add("row");
 
+    //using appendChild adds the element into the DOM otherwise it is just hanging out freely, not associated with something in the document tree
     div.appendChild(menuSubheading);
     div.appendChild(row);
 });
@@ -207,36 +209,76 @@ for (let i = 0; i < menuType.length; i++) {
     menuDivs[i].children[1].setAttribute("id", `${menuType[i]}Row`);
 }
 
-/* <img src="images/acksaltpop.jpg" alt="ackee and saltfish plantain poppers" class="img-fluid menu-img card-image-top"/>
+//populate menu items
+//put the food type rows into containers in the DOM
+
+const appRow = document.getElementById("appetizersRow");
+const entreesRow = document.getElementById("entreesRow")
+const drinksRow = document.getElementById("drinksRow");
+const dessertsRow = document.getElementById("dessertsRow");
+
+//take each object in the MenuItems array create a column div for the card to "live" in and then create a card div for the HMTL we created to occupy
+menuItems.forEach(item => {
+    const column = document.createElement("div");
+    column.classList.add("col-md-3");
+
+    const card = document.createElement("div");
+    card.classList.add("card", "h-100");
+    card.innerHTML = `
+    <img src="images/${item.imgUrl}" alt="${item.desc}" class="img-fluid menu-img card-image-top"/>
                     <div class="card-body">
-                        <h4 class="card-title text-capitalize item-item">Name of Item</h4>
-                        <p class="card-text text-uppercase item-desc">Desc of Item</p>
+                        <h4 class="card-title text-capitalize item-item">${item.item}</h4>
+                        <p class="card-text text-uppercase item-desc">${item.desc}</p>
                     </div>
                     <footer class="card-footer">
-                        <p class="card-text item-price">$dollars</p>
+                        <p class="card-text item-price">$${item.price}</p>
                         <div class="buttons-div d-flex">
                             <button 
                             class="btn btn-primary cart-btn text-capitalize"
-                            id="btnNumberOfItem"
-                            data-id=""
-                            data-price=""
-                            data-qty=""
-                            data-item=""
+                            id="Btn${item.id}"
+                            data-id="${item.id}"
+                            data-price="${item.price}"
+                            data-qty="${item.qty}"
+                            data-item="${item.item}"
                             >add to cart</button>
                             <div class="qty-div">
                                 <button
                                 class="btn btn-secondary btn-subtract"
-                                id="btnSubtractItemId"
-                                data-id=""
-                                data-qty=""
+                                id="btnSubtract${item.id}"
+                                data-id="${item.id}"
+                                data-qty="${item.qty}"
                                 > - </button>
-                                <span class="quantity" id="quantityItemId">Number</span>
+                                <span class="quantity" id="quantity${item.id}">${item.qty}</span>
                                 <button
                                 class="btn btn-secondary btn-add"
-                                id="btnAddItemId"
-                                data-id=""
-                                data-qty=""
+                                id="btnAdd${item.id}"
+                                data-id="${item.id}"
+                                data-qty="${item.qty}"
                                 > + </button>
                             </div>
                         </div>
-                    </footer> */
+                    </footer>
+    `
+    column.appendChild(card);
+
+    // we cannot use appRow.appendChild(column), because it will push all of the menu item cards into just one section. To make our program sort the cards into the correct category, we will use a switch case
+
+    switch (item.type) {
+        case "appetizers":
+            appRow.appendChild(column)
+            break;
+        case "entrees":
+            entreesRow.appendChild(column)
+            break;
+        case "drinks":
+            drinksRow.appendChild(column)
+            break;
+        case "desserts":
+            dessertsRow.appendChild(column)
+            break;
+        default:
+            console.log("Error: menu type not listed")
+            break;
+    }
+})
+
